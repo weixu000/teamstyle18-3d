45c01703fba1a5222562ebfb19f5766f7f5f133a
franklore <13051353880@163.com>
2017-02-26 18:15:23 +0800

    bling bling effect added

--- Assets/Scripts/Controls/PlaneControl.cs
+++ Assets/Scripts/Controls/PlaneControl.cs
@@ -5,8 +5,8 @@ public class PlaneControl : InvasiveControl {
     public override void Fire(int target_id)
     {
         fire.transform.rotation = Quaternion.LookRotation(GameObject.Find(target_id.ToString()).transform.position - fire.transform.position);
-        fire.SetActive(true);
         fire.GetComponent<XLine>().target = GameObject.Find(target_id.ToString());
+        fire.SetActive(true);
         base.Fire(target_id);
     }
 }
---
+++ Assets/Scripts/TreeRemover.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeRemover : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+       if (other.tag == "Tree" || other.tag == "Rock")
+          Destroy(other.gameObject);
+    }
+}
