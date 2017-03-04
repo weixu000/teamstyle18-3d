2313858a3f49e04f366eae158293db2ee752c5e7
weixu000 <wei-x15@mails.tsinghua.edu.cn>
2017-03-04 19:13:59 +0800

    alter load scene method

---
+++ Assets/UI/LoadScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public string sceneName;
+
+    public void loadScene()
+    {
+        SceneManager.LoadScene(sceneName);
+        Debug.Log(sceneName + " loaded");
+    }
+}
