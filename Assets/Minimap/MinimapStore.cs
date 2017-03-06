72e02e1a78692a66bd3dbe97d2397f3f05d894bf
weixu000 <wei-x15@mails.tsinghua.edu.cn>
2017-03-06 20:37:11 +0800

    add minimap

---
+++ Assets/Minimap/MinimapStore.cs
@@ -0,0 +1,5 @@
+﻿using UnityEngine;
+
+public class MinimapStore : MonoBehaviour {
+    public GameObject[] buildings, units;
+}
--- Assets/Scripts/Controls/UnitControl.cs
+++ Assets/Scripts/Controls/UnitControl.cs
@@ -7,12 +7,17 @@ public class UnitControl : MonoBehaviour
     public UnitType unit_type;
     public int flag = -1;
 
+    MinimapStore mapStore;
+    GameObject mapMark;
+
     UILabel posLabel, idLabel;
 
     protected virtual void Awake()
     {
         posLabel = transform.Find("Blood/Panel/PositionLabel").GetComponent<UILabel>();
         idLabel = transform.Find("Blood/Panel/IDLabel").GetComponent<UILabel>();
+
+        mapStore = GameObject.Find("MapCamera").GetComponent<MinimapStore>();
     }
 
     public virtual void SetState(UnitState state)
@@ -21,9 +26,31 @@ public class UnitControl : MonoBehaviour
         position = state.position;
         unit_name = state.unit_name;
         unit_type = state.unit_type;
-        flag = state.flag;
 
         posLabel.text = "(" + state.position.x + "," + state.position.y + ")";
         idLabel.text = "ID:" + name;
+
+        if (flag != state.flag)
+        {
+            if (mapMark)
+            {
+                Destroy(mapMark);
+            }
+
+            if (unit_type == UnitType.BUILDING)
+            {
+                mapMark = Instantiate(mapStore.buildings[state.flag + 1], transform, false);
+            }
+            else
+            {
+                mapMark = Instantiate(mapStore.units[state.flag], transform, false);
+            }
+        }
+
+        if (!mapMark)
+        {
+            mapMark = Instantiate(mapStore.buildings[0], transform, false);
+        }
+        flag = state.flag;
     }
 }
