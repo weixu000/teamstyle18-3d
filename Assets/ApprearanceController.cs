cbf534a2853cd23cdfa40e6280729e2f9b836f0d
franklore <13051353880@163.com>
2017-04-19 13:26:02 +0800

    added many things

---
+++ Assets/ApprearanceController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApprearanceController : MonoBehaviour {
+
+    public Clicker clicker;
+    public GameObject []items;
+    public GameObject selectedEffect;
+
+	// Use this for initialization
+	void Start () {
+        clicker = FindObjectOfType<Clicker>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (clicker.selectObject == gameObject)
+            foreach(GameObject a in items)
+            {
+                a.SetActive(true);
+                selectedEffect.SetActive(true);
+            }
+        else if (clicker.hitObject == gameObject)
+            foreach (GameObject a in items)
+            {
+                a.SetActive(true);       
+            }
+        else
+            foreach (GameObject a in items)
+            {
+                a.SetActive(false);
+                selectedEffect.SetActive(false);
+            }
+    }
+}
---
+++ Assets/ClickerFollower.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickerFollower : MonoBehaviour {
+
+    public Clicker clicker;
+    public float distance = 10;
+    public float height = 10;
+    public float followHeight = 3;
+    bool followed;
+ 
+	// Use this for initialization
+	void Start () {
+        clicker = FindObjectOfType<Clicker>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (followed)
+            {
+                followed = false;
+            }
+            else
+            {
+                followed = true;
+            }
+        }
+
+        if (followed == true)
+        {
+            if (clicker.selectObject == null)
+            {
+                followed = false;
+            }
+            else
+            {
+                Vector3 pos = new Vector3(
+                    clicker.selectObject.transform.position.x - distance * clicker.selectObject.transform.forward.x,
+                    height,
+                    clicker.selectObject.transform.position.z - distance * clicker.selectObject.transform.forward.z
+                    );
+                Vector3 uppos = new Vector3(0, followHeight, 0);
+                transform.position = pos;
+
+                transform.LookAt(clicker.selectObject.transform.position + uppos);
+            }
+        }
+
+	}
+}
