15600a232d340b575aec792a1dd184f03f051323
franklore <13051353880@163.com>
2017-04-30 17:29:19 +0800

    change building skills

---
+++ Assets/Archs/Cailiao/ShellDstroy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ShellDstroy : MonoBehaviour
+{
+    NetCom com;
+    int started;
+
+	void Awake () {
+        com = GameObject.Find("GameController").GetComponent<NetCom>();
+        started = com.round;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(com.round - started >= 10)
+        {
+            Destroy(this);
+        }
+	}
+}
